a663ca302ceee6a9085178ca61e3d1f64ad6137d
Abdulmaleek Mubaraq <mubaraqabdulmaleek@gmail.com>
2026-06-28 22:59:06 +0100

    first commit

---
+++ dotnetapp1/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿using PasswordGenerator;
+
+var pwd = new Password();
+var password = pwd.Next();
+
+Console.WriteLine($"Hello, here is your password: {password.ToString()}");
+// Console.WriteLine(password);
